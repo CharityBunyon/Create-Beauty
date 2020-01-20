3a0f98ed74c592ad2e5af3d4713b76a41e66ce54
Charity Bunyon <cseymou5@gmail.com>
2020-01-20 12:07:19 -0600

    auth setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,64 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Redirect, Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConnection from '../helpers/data/connection';
 import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-        <button className='btn btn-danger'>ATTACK</button>
-    </div>
-  );
+import Auth from '../components/pages/Auth/Auth';
+import Home from '../components/pages/Home/Home';
+import SingleLook from '../components/pages/SingleLook/SingleLook';
+
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+firebaseConnection();
+
+class App extends React.Component {
+  state = {
+    authed: true,
+    authed: false,
+  }
+
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
+      <div>
+        <Router>
+          <Switch>
+          <PublicRoute path="/auth" exact component={Auth} authed={authed}/>
+          <PrivateRoute path="/" exact component={Home} authed={authed}/>
+          <PrivateRoute path="/look/:lookId" exact component={SingleLook} authed={authed} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;

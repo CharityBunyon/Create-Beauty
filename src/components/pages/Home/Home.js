f9c8985ce0a9cf5c1d7d567b5a18faceb58c7158
Charity Bunyon <cseymou5@gmail.com>
2020-01-31 00:35:46 -0600

    can now filter through looks

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -10,7 +10,8 @@ import Looks from '../../shared/Looks/Looks';
 class Home extends React.Component {
   state = {
     looks: [],
-    ratings: [],
+    filteredLooks: [],
+    searchedLook: '',
   }
 
   componentDidMount() {
@@ -30,17 +31,37 @@ class Home extends React.Component {
       .catch((err) => console.error('error deleting look', err));
   }
 
+  filterLooks = (e) => {
+    const input = e.target.value.toUpperCase();
+    if (e.target.value !== '') {
+      const newLooks = [];
+      const newLooksList = this.state.looks.filter((look) => look.rating.toUpperCase().search(input) !== -1);
+      newLooks.push(newLooksList);
+      this.setState({ looks: newLooksList });
+    } else {
+      this.getLooks();
+    }
+  }
+
+
   render() {
-    const { userObj } = this.props;
-    // console.log(this.props.userObj);
-
     return (
-      <div className="Home text-center">
-
-      <h1 className="welcome">Welcome, {userObj.displayName}</h1>
+      <div className="Home">
+        <div className="searchArea">
+        <label className="beatLabel">Search By Beat Rating: </label>
+        <input
+          type="text"
+          className="searchBox"
+          placeholder="Search"
+          value={this.searchedLook}
+          onChange={this.filterLooks}
+        />
+        </div>
+        <div className="text-center">
         <Link className="btn btn-outline-dark btn-lg createBtn" to="/look/new">Create A Look</Link>
         <div className="looks container-fluid d-flex flex-wrap">
           {this.state.looks.map((look) => <Looks key={look.id} look={look} deleteLook={this.deleteLook} />)}
+        </div>
         </div>
       </div>
     );

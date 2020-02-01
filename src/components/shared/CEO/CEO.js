f88573d8c87ded7e43901765dd432a4200bff436
Charity Bunyon <cseymou5@gmail.com>
2020-02-01 15:16:15 -0600

    deployed site

--- src/components/shared/CEO/CEO.js
+++ src/components/shared/CEO/CEO.js
@@ -6,7 +6,7 @@ class CEO extends React.Component {
     return (
       <div className="container-fluid d-flex ceoDiv">
         <div className="col text-center">
-          <img className="ceoImg" src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/83702414_10220217795178118_2414406455173054464_o.jpg?_nc_cat=103&_nc_oc=AQnyah_k6BcZTG-t1bAG0NDHbg1gCoY_DqcPW31Zn4iMCCNGB5tWLFwgEmzxp1krHG5bmlurWyQ77DXuvfiT7DyV&_nc_ht=scontent-atl3-1.xx&oh=8495608c99e9818c94f8fcb6c9aeb53e&oe=5E91E771" alt="" />
+          <img className="ceoImg" src="https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/p720x720/83909849_10220217453049565_8872782266028261376_o.jpg?_nc_cat=100&_nc_oc=AQmIO8FkxehjA6tdQWueWJtcPBNCs4HuFTPdk4OQjg6PDJH4unDdMR4KG69OqjW1gJc&_nc_ht=scontent-ort2-2.xx&_nc_tp=6&oh=2a5b79bba6524804262d36f459aa9e69&oe=5EBFEBB9" alt="" />
         </div>
         <div className="col mainStory">
           <h2 className="quoteTitle">EXPRESS YOURSELF</h2>

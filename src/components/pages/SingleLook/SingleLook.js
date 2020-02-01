411da8042f8fa05f4b0ddd4f2590505f3bd11d31
Charity Bunyon <cseymou5@gmail.com>
2020-02-01 16:07:27 -0600

    fixed styling for singleLook

--- src/components/pages/SingleLook/SingleLook.js
+++ src/components/pages/SingleLook/SingleLook.js
@@ -22,28 +22,32 @@ class SingleLook extends React.Component {
     const { look } = this.state;
 
     return (
-      <div className="container singleLookView">
+      <div className="container-fluid singleLookView">
       <div className="singleLookBtns text-center">
      <Link className="btn btn-outline-dark btn-lg createBtn" to="/">CLOSE</Link>
       </div>
 
-      <div className="row d-flex">
-        <div className="col text-center">
+      <div className="d-flex singleLookImgDiv">
+        <div className="text-center col">
           <img className="card-img-top lookImg" src={look.imgUrl} alt=""/>
             <div>
-              <p className="beatRating">BEAT RATING: {look.rating}</p>
+              <p className="beatRating">Beat Rating: {look.rating}</p>
             </div>
         </div>
-            <div className="col">
-              <div className="lookSteps">
-                <p className="rouProTitle">The Routine:</p>
-                <p className="rouProPara">{look.steps}</p>
-              </div>
-          <div>
-            <p className="rouProTitle">The Products:</p>
-            <p className="rouProPara">{look.products}</p>
+
+          <div className="col singleLookDeets">
+            <div className="lookSteps">
+              <p className="rouProTitle">The Routine:</p>
+              <p className="rouProPara">{look.steps}</p>
+            </div>
           </div>
-        </div>
+
+          <div className="col singleLookDeets">
+            <div className="lookSteps">
+              <p className="rouProTitle">The Products:</p>
+              <p className="rouProPara">{look.products}</p>
+            </div>
+          </div>
       </div>
       </div>
     );

70931802bb5327fe498035d88082c47a4d0d00f3
Charity Bunyon <cseymou5@gmail.com>
2020-06-05 07:44:37 -0500

    fixed ceo image and models div

--- src/components/shared/CEO/CEO.js
+++ src/components/shared/CEO/CEO.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CEO.scss';
+import me from '../../../Images/ceo.jpg';
 
 class CEO extends React.Component {
   // I made a CEO component when I'm rendering the domstring below
@@ -8,7 +9,7 @@ class CEO extends React.Component {
     return (
         <div className="row ceoDiv">
           <div className="col text-center">
-            <img className="ceoImg" src="https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/p720x720/83909849_10220217453049565_8872782266028261376_o.jpg?_nc_cat=100&_nc_oc=AQmIO8FkxehjA6tdQWueWJtcPBNCs4HuFTPdk4OQjg6PDJH4unDdMR4KG69OqjW1gJc&_nc_ht=scontent-ort2-2.xx&_nc_tp=6&oh=2a5b79bba6524804262d36f459aa9e69&oe=5EBFEBB9" alt="" />
+            <img className="ceoImg" src= {me}/>
           </div>
           <div className="col mainStory text-center">
             <h2 className="quoteTitle">EXPRESS YOURSELF</h2>
